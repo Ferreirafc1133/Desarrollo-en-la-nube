0fa6780a8bfbdd0708cf287c0aae9c3ba9149a3c
ferreira <ferreira@boom.ai>
2024-09-28 00:45:08 -0600

    Update views.py

--- taskHorizonDJ/tasks/views.py
+++ taskHorizonDJ/tasks/views.py
@@ -17,6 +17,7 @@ import json
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tasks_table = dynamodb.Table('Tareas')
 files_table = dynamodb.Table('Archivos')
+s3 = boto3.client('s3', region_name='us-east-1')
 
 # Ver lista de tareas
 def list_tasks(request):
@@ -61,7 +62,6 @@ def create_task(request):
 
             for archivo in archivos:
                 if archivo.size <= 10 * 1024 * 1024:
-                    s3 = boto3.client('s3')
                     bucket_name = 'tareasextra'
                     file_name = archivo.name
                     nombre_archivo = f"{file_name}_{fecha_subida}"
@@ -144,7 +144,6 @@ def update_task(request, task_id):
             has_updates = True
 
         if archivo:
-            s3 = boto3.client('s3')
             bucket_name = 'tareasextra'
             file_name = archivo.name
             fecha_subida = timezone.now().strftime('%Y%m%d_%H%M%S')
@@ -220,7 +219,6 @@ def delete_task(request, task_id):
             archivos = files_response.get('Items', [])
 
             if archivos:
-                s3 = boto3.client('s3')
                 for archivo in archivos:
                     try:
                         s3.delete_object(Bucket='tareasextra', Key=archivo['nombre_archivo'])
