cd2bcc7d1ff73fb6285d4b17d3776a92c1a08df5
ferreira <ferreira@boom.ai>
2024-09-30 00:28:50 -0600

    fix

--- examenpractProy/examenpractProy/urls.py
+++ examenpractProy/examenpractProy/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views_clientes
 
 urlpatterns = [
-    path('clientes/', views.cliente_list, name='cliente_list'),
-    path('clientes/crear/', views.cliente_create, name='cliente_create'),
-    path('clientes/<str:id>/', views.cliente_detail, name='cliente_detail'),
-    path('clientes/<str:id>/editar/', views.cliente_update, name='cliente_update'),
-    path('clientes/<str:id>/eliminar/', views.cliente_delete, name='cliente_delete'),
+    path('clientes/', views_clientes.cliente_list, name='cliente_list'),
+    path('clientes/crear/', views_clientes.cliente_create, name='cliente_create'),
+    path('clientes/<str:id>/', views_clientes.cliente_detail, name='cliente_detail'),
+    path('clientes/<str:id>/editar/', views_clientes.cliente_update, name='cliente_update'),
+    path('clientes/<str:id>/eliminar/', views_clientes.cliente_delete, name='cliente_delete'),
 ]
