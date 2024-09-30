8c661513735f436a9546c167d5ccb3940ca4606a
ferreira <ferreira@boom.ai>
2024-09-30 00:36:26 -0600

    Update urls.py

--- examenpractProy/examenpractProy/urls.py
+++ examenpractProy/examenpractProy/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from examenpractApp  import views_clientes
 
+print("Cargando el archivo urls.py")
+
 urlpatterns = [
     path('clientes/', views_clientes.cliente_list, name='cliente_list'),
     path('clientes/crear/', views_clientes.cliente_create, name='cliente_create'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('clientes/<str:id>/editar/', views_clientes.cliente_update, name='cliente_update'),
     path('clientes/<str:id>/eliminar/', views_clientes.cliente_delete, name='cliente_delete'),
 ]
+
+print("Rutas cargadas:", urlpatterns)
