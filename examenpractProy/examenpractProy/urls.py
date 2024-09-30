4428b6f86528a9b5669a1289569fefea8b902cd1
ferreira <ferreira@boom.ai>
2024-09-30 00:33:43 -0600

    fix

--- examenpractProy/examenpractProy/urls.py
+++ examenpractProy/examenpractProy/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views_clientes
+from examenpractApp  import views_clientes
 
 urlpatterns = [
     path('clientes/', views_clientes.cliente_list, name='cliente_list'),
